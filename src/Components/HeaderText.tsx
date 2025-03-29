17b1f887343fb2fc85faa3233dc7431c0a17df91
feranmiola <osunjuyigbeiyin@gmail.com>
2025-03-29 11:30:08 +0100

    Refactor global styles and update homepage layout. Removed unused CSS variables and added Poppins font. Simplified the page structure by integrating HeaderText component and streamlining the layout.

---
+++ src/Components/HeaderText.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const HeaderText = () => {
+  return (
+    <div className="max-w-[370px] max-h-[324px] flex flex-col justify-between">
+      <p className="font-bold text-[72px] font-poppins text-[#FEC3C7]">
+        Design.
+      </p>
+      <p className="font-bold text-[72px] font-poppins text-[#9AD7F3]">
+        Create
+      </p>
+      <p className="font-bold text-[72px] font-poppins text-[#6BDFDA]">
+        Inspire.
+      </p>
+    </div>
+  );
+};
+
+export default HeaderText;
