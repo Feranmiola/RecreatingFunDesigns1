b9b0e2c1163ecf870859a2a3df98988939d7149c
feranmiola <osunjuyigbeiyin@gmail.com>
2025-03-29 13:32:32 +0100

    Update HeaderText component to include gradient hover effect. Enhance global styles with new gradient-text-hover class for improved text interaction on hover.

--- src/Components/HeaderText.tsx
+++ src/Components/HeaderText.tsx
@@ -3,13 +3,13 @@ import React from "react";
 const HeaderText = () => {
   return (
     <div className="max-w-[370px] max-h-[324px] flex flex-col justify-between">
-      <p className="font-bold text-[72px] font-poppins text-[#FEC3C7]">
+      <p className="font-bold text-[72px] font-poppins text-[#FEC3C7] gradient-text-hover">
         Design.
       </p>
-      <p className="font-bold text-[72px] font-poppins text-[#9AD7F3]">
+      <p className="font-bold text-[72px] font-poppins text-[#9AD7F3] gradient-text-hover">
         Create
       </p>
-      <p className="font-bold text-[72px] font-poppins text-[#6BDFDA]">
+      <p className="font-bold text-[72px] font-poppins text-[#6BDFDA] gradient-text-hover">
         Inspire.
       </p>
     </div>
