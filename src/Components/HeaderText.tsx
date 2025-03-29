27ee156bd648b5f9f6285378f163d27d159d9cba
feranmiola <osunjuyigbeiyin@gmail.com>
2025-03-29 14:44:05 +0100

    Refactor HeaderText component to incorporate Framer Motion for animated text effects. Replace static text with animated elements for a more engaging user experience.

--- src/Components/HeaderText.tsx
+++ src/Components/HeaderText.tsx
@@ -1,18 +1,63 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const HeaderText = () => {
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.5,
+      },
+    },
+  };
+
+  const wordContainerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.1, // Delay between each letter
+      },
+    },
+  };
+
+  const letterVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.3,
+        ease: "easeOut",
+      },
+    },
+  };
+
+  const AnimatedText = ({ text, color }: { text: string; color: string }) => (
+    <motion.p
+      className={`font-bold w-max text-[72px] font-poppins ${color} gradient-text-hover`}
+      variants={wordContainerVariants}
+    >
+      {text.split("").map((char, index) => (
+        <motion.span key={index} variants={letterVariants}>
+          {char}
+        </motion.span>
+      ))}
+    </motion.p>
+  );
+
   return (
-    <div className="max-w-[370px] max-h-[324px] flex flex-col justify-between">
-      <p className="font-bold w-max text-[72px] font-poppins text-[#FEC3C7] gradient-text-hover">
-        Design.
-      </p>
-      <p className="font-bold w-max text-[72px] font-poppins text-[#9AD7F3] gradient-text-hover">
-        Create
-      </p>
-      <p className="font-bold w-max text-[72px] font-poppins text-[#6BDFDA] gradient-text-hover">
-        Inspire.
-      </p>
-    </div>
+    <motion.div
+      className="max-w-[370px] max-h-[324px] flex flex-col justify-between"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+    >
+      <AnimatedText text="Design." color="text-[#FEC3C7]" />
+      <AnimatedText text="Create." color="text-[#9AD7F3]" />
+      <AnimatedText text="Inspire." color="text-[#6BDFDA]" />
+    </motion.div>
   );
 };
 
